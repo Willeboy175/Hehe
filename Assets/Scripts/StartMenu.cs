4d8d353e93d14e91fb766d0ec4db9c702785e1f6
Willeboy175 <william.strandberg@elev.ga.lbs.se>
2024-02-14 13:45:44 +0100

    Groundwork complete

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -1,31 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public virtual void Yes()
     {
         SceneManager.LoadScene("Marry");
     }
 
+    public void Yes2()
+    {
+        SceneManager.LoadScene("Yaaay");
+    }
+
     public virtual void No()
     {
-        int rng = Mathf.RoundToInt(Random.Range(0, 4));
+        int rng = 0;
+        string newScene = "Scene";
+        string currentScene = "Scene";
+
+        while (newScene == currentScene)
+        {
+            rng = Mathf.RoundToInt(Random.Range(0, 4));
+            newScene = "Scene" + rng;
+            currentScene = SceneManager.GetActiveScene().name;
+            print("newScene: " + newScene + " and " + "currentScene " + currentScene);
+        }
 
         SceneManager.LoadScene("Scene" + rng);
     }
